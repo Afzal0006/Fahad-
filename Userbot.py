a4a86f7901e71d8fb7f4f27a868c89bad11714d3
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-08-02 22:13:53 +0530

    Update Userbot.py

--- Userbot.py
+++ Userbot.py
@@ -1,8 +1,8 @@
 from telethon import TelegramClient, events
 
-API_ID = 123456
-API_HASH = "your_api_hash"
-SESSION = "userbot_session"
+API_ID = 26014459
+API_HASH = "34b8791089c72367a5088f96d925f989"
+SESSION = "BQGM8vsALiR2AWik4SWrvYlsaRw9hdOF82r43DkMpxY9QyxZn3s1dSfff7vUeyN_SrcoJESXDSSwtWx2wnv_F7hzeEKuFRh6C_VnmTChySSxagfKdV7tQvCgz0sOFxiSvUipmsLod7rSxTC8OUM35KGV4bjyvRewPO-WhPlYFsnQUxoG4dOxNHW9MwDr-UToR097V5byYZryELXttdF4NlycFKZo34Gs6qGnpQ08M887g9Bc4cr-eA6iq-_9qx_BCGSiHGL8SLkS25XAmio5AMnPBOatKUqQNBW_BbAr9KXOjtV2uJRXx6znm08TmEB45iD_rzyvQkT1SrNcSknR0RDA8lAFDgAAAAGx20OoAA"
 
 client = TelegramClient(SESSION, API_ID, API_HASH)
 
