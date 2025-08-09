fdc9296ef3f7fd71f280844ba3294c9842a83b01
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-08-10 04:09:39 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -6,7 +6,12 @@ API_ID = 26014459
 API_HASH = "34b8791089c72367a5088f96d925f989"
 STRING_SESSION = "BQGM8vsAJVppG5SfjCvycz5l9o_UIsYpj3bvjYYF7qxZijHTM8_7mx8HlI2NVksjHXC3o31_QhFdq3VQGp510kRTE8CP0lYNSxQoM7A00-Wa56JNH1R2cNWTDuUGTYXqbif1B4z96_vPRJvPysL-R-6YMO7BDrI39Poyxv-IieogpMorJKUiQEgn1DjbeQTQNkpbJNwa2l-sbXumBfw5zwMCCZo4-iW_cNULOJLR_hw9-cRC64tMvegiJUUxmpweOThIJdz4ElEl7_qWV1HJSuTkPHyO_RaAIem-GwqQEi5RUlfpKXkCcOZYkPzZpMyrymLzcD0c-cGjPY7lqvFatJnNxF__VwAAAAGx20OoAA"
 
-app = Client("my_userbot", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
+app = Client(
+    "my_userbot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=STRING_SESSION
+)
 
 broadcast_task = None
 stop_broadcast = False
@@ -75,7 +80,7 @@ async def periodic_broadcast(client, message_text, delay):
             try:
                 await client.send_message(dialog.chat.id, message_text)
                 sent += 1
-            except:
+            except Exception:
                 failed += 1
         print(f"Broadcast sent to {sent} chats, failed in {failed} chats.")
         await asyncio.sleep(delay)
